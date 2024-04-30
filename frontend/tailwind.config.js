fa1d16f2ebb1fc20cfbbb2bc7b9bdf8a012fc97a
detaomega <cosmolynnyang@gmail.com>
2024-05-01 05:58:42 +0800

    feat: set tailwind css config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,ts}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  variants: {
+    extend: {},
+  },
+  plugins: [
+  ],
 }
