378d5aaa11a01e290a267b7608150b4cb5571fe8
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2024-05-03 01:48:55 +0800

    feat: user login

--- frontend/src/service/userLoginService.ts
+++ frontend/src/service/userLoginService.ts
@@ -1,19 +1,19 @@
-import type { user } from '../types/user';
+import type { user } from '../types/user'
 
 export default class userLoginService {
-  static async userLogin(user_name: string, user_password: string): Promise<user> {
-    // const response = await fetch('/login', {
-    //   method: 'POST',
-    //   headers: {
-    //       'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({ user_account, user_password })
-    // });
-    // const result = await response.json();
+  static async userLogin(user_account: string, user_password: string): Promise<user> {
+    console.log(user_account, user_password)
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user_account: user_account, user_password: user_password })
+    })
+    const result = await response.json()
     return {
-      outh_token: '123456',
-      user_identity: 'invalid'
+      outh_token: result.outh_token,
+      user_identity: result.user_identity
     }
   }
 }
-  
