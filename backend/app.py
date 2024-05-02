89a4edf37d5081aa4ba1ada0de8fd0d210f1c076
cy0802 <amyyoyo0802@gmail.com>
2024-05-02 23:28:11 +0800

    backend login

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from models import db, Staff_Information
+import pymysql
+import dotenv
+import os
 
+dotenv.load_dotenv()
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+db_user = os.getenv("MYSQL_USER")
+db_password = os.getenv("MYSQL_PASSWORD")
+db_host = os.getenv("MYSQL_HOST")
+db_port = os.getenv("MYSQL_PORT")
+db_name = os.getenv("MYSQL_DATABASE")
 
-@app.route('/test')
-def test():
-    return jsonify({'message': 'hello world!'})
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+db.init_app(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    account = request.form.get("user_account")
+    password = request.form.get("user_password")
+    staff = Staff_Information.query.filter_by(Gmail=account, Password=password).first()
+    if staff is None:
+        return jsonify({'outh_token': "", 'user_identity': "invalid_user"})
+    return jsonify({'outh_token': "", 'user_identity': staff.Position})
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
--- db/init.sql
+++ db/init.sql
@@ -3,9 +3,10 @@ CREATE TABLE Staff_Information(
     StaffName VARCHAR(255),
     Position VARCHAR(255),
     Gmail VARCHAR(255),
+    Password VARCHAR(255),
     PhoneNumber BIGINT
 );
-INSERT INTO Staff_Information (StaffID, StaffName, Position, Gmail, PhoneNumber) VALUES 
-    (100001, 'ycy.yo', 'clerk', 'ycy.yo@gmail.com', '0909090909'), 
-    (100002, 'amber chen', 'admin', 'hello@world', '0910101010'),
-    (100003, 'whoami', 'customer', 'who@ami', '0911111111');
+INSERT INTO Staff_Information (StaffID, StaffName, Position, Gmail, Password, PhoneNumber) VALUES 
+    (100001, 'ycy.yo', 'clerk', 'ycy.yo@gmail.com', 'test', 0909090909), 
+    (100002, 'amber chen', 'manager', 'hello@world', 'test', 0910101010),
+    (100003, 'whoami', 'worker', 'who@ami', 'test', 0911111111);
