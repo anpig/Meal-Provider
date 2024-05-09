a31346c610beaf051cdf5cb0a6fb4af5a1661da1
Bruce-0015 <brucelin.cs09@nycu.edu.tw>
2024-05-10 00:06:36 +0800

    feat: restaurant type

---
+++ frontend/src/types/restaurant.ts
@@ -0,0 +1,13 @@
+interface meal {
+    name: string
+    type: string
+    price: number
+    description: string
+}
+
+interface restaurant {
+    restaurant: string
+    meals: meal[]
+}
+
+export { type restaurant, type meal }
