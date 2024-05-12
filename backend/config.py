b4e8b5b5aa38bb7742991b12c2b83163e18d9a41
cy0802 <amyyoyo0802@gmail.com>
2024-05-12 23:14:29 +0800

    fix: config db_host

--- backend/config.py
+++ backend/config.py
@@ -10,6 +10,6 @@ db_host = os.getenv("MYSQL_HOST")
 db_port = os.getenv("MYSQL_PORT")
 db_name = os.getenv("MYSQL_DATABASE")
 
-db_host = 'localhost'
+# db_host = 'localhost'
 
 SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
